f692b20a0fd1203dfd2080aa8686ca6ef4718f4a
alan.invents <alan.invents@gmail.com>
2020-05-24 14:58:34 -0400

    Added and improved `string!` actions

--- runtime/datatypes/string.ts
+++ runtime/datatypes/string.ts
@@ -41,6 +41,20 @@ export function $$mold(
 
 // ...
 
+export function $$copy(
+	_ctx: Red.Context,
+	str:  Red.RawString,
+	_: RedActions.CopyOptions = {}
+): Red.RawString {
+	if(_.part !== undefined) {
+		Red.todo();
+	} else if(str.index == 1) {
+		return new Red.RawString([...str.values]);
+	} else {
+		return new Red.RawString(str.values.slice(str.index - 1));
+	}
+}
+
 export function $$append(
 	ctx:   Red.Context,
 	str:   Red.RawString,
@@ -53,9 +67,39 @@ export function $$append(
 		str.values.push(value);
 	} else if(value instanceof Red.RawString) {
 		str.values.push(...value.current().values);
+	} else if(value instanceof Red.RawBlock) {
+		for(const elem of value.current().values) {
+			str.values.push(...RedActions.$$form(ctx, elem).values);
+		}
 	} else { // Unsure if /only should be ignored
 		str.values.push(...RedActions.$$form(ctx, value).values);
 	}
 
 	return str;
+}
+
+export function $$poke(
+	_ctx:  Red.Context,
+	str:   Red.RawString,
+	index: Red.AnyType,
+	value: Red.AnyType
+): Red.RawChar|Red.RawInteger {
+	if(!(index instanceof Red.RawInteger)) {
+		throw new TypeError("error!");
+	}
+	
+	if(index.value < 1 || index.value > str.length) {
+		throw new RangeError("error!");
+	} else if(value instanceof Red.RawChar) {
+		return str.values[(str.index - 1) + (index.value - 1)] = value;
+	} else if(value instanceof Red.RawInteger) {
+		if(value.value < 0) {
+			throw new RangeError("Out of bounds!");
+		} else {
+			str.values[(str.index - 1) + (index.value - 1)] = new Red.RawChar(value.value);
+			return value;
+		}
+	} else {
+		throw new TypeError("error!");
+	}
 }
