5d16b095d2f6d12594bc762016aee32c302ba4f9
theangryepicbanana <alan.invents@gmail.com>
2025-03-15 17:55:06 -0400

    Fix print bug

--- src/runtime/natives/Print.hx
+++ src/runtime/natives/Print.hx
@@ -2,13 +2,19 @@ package runtime.natives;
 
 import types.Value;
 import types.Unset;
+import types.Block;
 
 import runtime.actions.Form;
 
 @:build(runtime.NativeBuilder.build())
 class Print {
 	public static function call(value: Value) {
-		RedJS.printHandler(Form.call(Reduce.call(value, Reduce.defaultOptions), Form.defaultOptions).toJs());
+		RedJS.printHandler(
+			Form.call(
+				if(value is Block) Reduce.call(value, Reduce.defaultOptions) else value,
+				Form.defaultOptions
+			).toJs()
+		);
 
 		return Unset.UNSET;
 	}
